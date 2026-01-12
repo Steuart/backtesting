b8363541878ac42aecba599ac58dda30b3e2b8ce
Ming <jianmo_o@163.com>
2026-01-12 11:36:30 +0800

    feat(交易策略): 添加交易执行和状态通知功能
    在LearnStrategy类中实现交易操作和通知功能：
    - 添加现金余额查询和交易操作
    - 实现订单状态通知回调
    - 添加交易状态更新日志记录

--- src/strategy/learn_strategy.py
+++ src/strategy/learn_strategy.py
@@ -14,7 +14,28 @@ class LearnStrategy(bt.Strategy):
 
     
     def next(self):
-        self.log(f'data1 close: {self.data_map['data1'].close[0]}, data2 close: {self.data_map['data2'].close[0]}')
+        cash = self.broker.getcash()
+        self.log(f'cash: {cash}')
+        data1 = self.data_map['data1']
+        data2 = self.data_map['data2']
+        self.buy(data=data2, size=100)
+        self.sell(data=data2, size=100)
+
+    def notify_order(self, order):
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(f'BUY EXECUTED, Price: {order.executed.price:.2f}, Cost: {order.executed.value:.2f}, Comm: {order.executed.comm:.2f}')
+            elif order.issell():
+                self.log(f'SELL EXECUTED, Price: {order.executed.price:.2f}, Cost: {order.executed.value:.2f}, Comm: {order.executed.comm:.2f}')
+    
+    def notify_trade(self, trade):
+        if trade.isclosed:
+            self.log(f"TRADE CLOSED, Profit: {trade.pnl:.2f}")
+        elif trade.isopen:
+            self.log(f"TRADE OPENED, Size: {trade.size}")
+        else:
+            # 持仓中盈亏更新
+            self.log(f"TRADE UPDATED, PnL: {trade.pnl:.2f}")
 
     def log(self, txt, dt=None):
         dt = dt or self.datas[0].datetime.date(0)
